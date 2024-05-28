779f0c8828daad06b0544c6f74077d1591ed5ca6
harshvardhan-personal <harshvardhansinhjadeja49@gmail.com>
2024-05-28 15:05:10 +0530

    created iam.tf file for iam role

--- Infrastructure/stack/main.tf
+++ Infrastructure/stack/main.tf
@@ -8,11 +8,3 @@ terraform {
 
   required_version = "~> 1.7"
 }
-
-# Fetching IAM Role for Task execution
-data "aws_iam_role" "task_exec-role" {
-  //for personal
-  name = "ecsTaskExecutionRole"
-  //for sandbox
-  # name = "AmazonECSTaskExecutionRole"
-}
